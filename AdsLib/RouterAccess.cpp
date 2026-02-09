taxmann-bhs/ADS

commit 7a35dd332dcfcca134658423a8553bbb4e86edb3

AdsTool: support pciscan
This feature is handy to test TwinCATs PCI drivers.

Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

AdsLib/RouterAccess.cpp
@@ -1,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+/**
+   Copyright (c) 2021 Beckhoff Automation GmbH & Co. KG
+ */
+
+#include "RouterAccess.h"
+#include "Log.h"
+#include "wrap_endian.h"
+#include <array>
+#include <iostream>
+
+namespace bhf
+{
+namespace ads
+{
+struct SearchPciBusReq {
+    SearchPciBusReq(const uint64_t quad)
+        : leVendorID(bhf::ads::htole<uint16_t>(quad >> 48))
+        , leDeviceID(bhf::ads::htole<uint16_t>(quad >> 32))
+        , leSubVendorID(bhf::ads::htole<uint16_t>(quad >> 16))
+        , leSubSystemID(bhf::ads::htole<uint16_t>(quad))
+    {}
+private:
+    const uint16_t leVendorID;
+    const uint16_t leDeviceID;
+    const uint16_t leSubVendorID;
+    const uint16_t leSubSystemID;
+};
+
+struct SearchPciSlotResNew {
+    static constexpr size_t MAXBASEADDRESSES = 6;
+    const std::array<uint32_t, MAXBASEADDRESSES> leBaseAddresses;
+    const uint32_t leSize[MAXBASEADDRESSES];
+    const uint32_t leBusNumber;
+    const uint32_t leSlotNumber;
+    const uint16_t leBoardIrq;
+    const uint16_t lePciRegViaPorts;
+};
+
+struct SearchPciBusResNew {
+    static constexpr size_t MAXSLOTRESPONSE = 64;
+    uint32_t leFound;
+    std::array<SearchPciSlotResNew, MAXSLOTRESPONSE> slot;
+    uint32_t nFound() const
+    {
+        return bhf::ads::letoh(leFound);
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const SearchPciSlotResNew& slot)
+{
+    return os << std::dec << bhf::ads::letoh(slot.leBusNumber) << ':' <<
+           bhf::ads::letoh(slot.leSlotNumber) << " @ 0x" <<
+           bhf::ads::letoh(slot.leBaseAddresses[0]);
+}
+
+RouterAccess::RouterAccess(const std::string& gw, const AmsNetId netid, const uint16_t port)
+    : device(gw, netid, port ? port : 1)
+{}
+
+bool RouterAccess::PciScan(const uint64_t pci_id, std::ostream& os) const
+{
+#define ROUTERADSGRP_ACCESS_HARDWARE 0x00000005
+#define ROUTERADSOFFS_A_HW_SEARCHPCIBUS 0x00000003
+
+    SearchPciBusResNew res {};
+    uint32_t bytesRead;
+
+    const auto req = SearchPciBusReq {pci_id};
+    const auto status = device.ReadWriteReqEx2(
+        ROUTERADSGRP_ACCESS_HARDWARE,
+        ROUTERADSOFFS_A_HW_SEARCHPCIBUS,
+        sizeof(res), &res,
+        sizeof(req), &req,
+        &bytesRead
+        );
+
+    if (ADSERR_NOERR != status) {
+        LOG_ERROR(__FUNCTION__ << "(): failed with: 0x" << std::hex << status << '\n');
+        return false;
+    }
+
+    if (res.slot.size() < res.nFound()) {
+        LOG_WARN(__FUNCTION__
+                 << "(): data seems corrupt. Slot count 0x" << std::hex << res.nFound() << " exceedes maximum 0x" << res.slot.size() <<
+                 " -> truncating\n");
+    }
+
+    auto limit = std::min<size_t>(res.slot.size(), res.nFound());
+    os << "PCI devices found: " << std::dec << limit << '\n';
+    for (const auto& slot: res.slot) {
+        if (!limit--) {
+            break;
+        }
+        os << slot << '\n';
+    }
+    return !os.good();
+}
+}
+}

AdsLib/RouterAccess.h
@@ -1,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+/**
+   Copyright (c) 2021 Beckhoff Automation GmbH & Co. KG
+ */
+
+#pragma once
+
+#include "AdsDevice.h"
+
+namespace bhf
+{
+namespace ads
+{
+struct RouterAccess {
+    RouterAccess(const std::string& gw, AmsNetId netid, uint16_t port);
+    bool PciScan(uint64_t pci_id, std::ostream& os) const;
+private:
+    AdsDevice device;
+};
+}
+}

AdsTool/main.cpp
@@ -9,10 +9,12 @@
 #include "AdsLib.h"
 #include "LicenseAccess.h"
 #include "Log.h"
+#include "RouterAccess.h"
 #include "RTimeAccess.h"
 #include "ParameterList.h"
 #include <cstring>
 #include <iostream>
+#include <limits>
 #include <unistd.h>
 #include <vector>
 
@@ -89,6 +91,15 @@ COMMANDS:
 	netid
 		Read the AmsNetId from a remote TwinCAT router
 		$ adstool 192.168.0.231 netid
+
+	pciscan <pci_id>
+		Show PCI devices with <pci_id>
+	examples:
+		List PCI CCAT devices:
+		$ adstool 5.24.37.144.1.1 pciscan 0x15EC5000
+		PCI devices found: 2
+		3:0 @ 0x4028629004
+		7:0 @ 0x4026531852
 
 	raw [--read=<number_of_bytes>] <IndexGroup> <IndexOffset>
 		This command gives low level access to:
@@ -273,6 +284,18 @@ int RunNetId(const IpV4 remote)
     bhf::ads::GetRemoteAddress(remote, netId);
     std::cout << netId << '\n';
     return 0;
+}
+
+int RunPCIScan(const AmsNetId netid, const uint16_t port, const std::string& gw, bhf::Commandline& args)
+{
+    const auto device = bhf::ads::RouterAccess{ gw, netid, port };
+    auto pciId = args.Pop<uint64_t>("pciscan pci_id is missing");
+
+    /* allow subVendorId/SystemId to be omitted from cmd */
+    if (std::numeric_limits<uint32_t>::max() >= pciId) {
+        pciId <<= 32;
+    }
+    return device.PciScan(pciId, std::cout);
 }
 
 int RunRTime(const AmsNetId netid, const uint16_t port, const std::string& gw, bhf::Commandline& args)
@@ -551,6 +574,7 @@ int ParseCommand(int argc, const char* argv[])
     const auto commands = CommandMap {
         {"file", RunFile},
         {"license", RunLicense},
+        {"pciscan", RunPCIScan},
         {"raw", RunRaw},
         {"rtime", RunRTime},
         {"state", RunState},

meson.build
@@ -12,6 +12,7 @@ common_files = files([
   'AdsLib/AdsFile.cpp',
   'AdsLib/LicenseAccess.cpp',
   'AdsLib/Log.cpp',
+  'AdsLib/RouterAccess.cpp',
   'AdsLib/RTimeAccess.cpp',
   'AdsLib/Sockets.cpp',
   'AdsLib/Frame.cpp',

tools/91_test_AdsTool.sh
@@ -59,6 +59,10 @@ check_license() {
 	fi
 }
 
+check_pciscan() {
+	check pciscan 0x15EC5000
+}
+
 check_raw() {
 	local _test_string
 	_test_string="$(date +%F%T)"
@@ -168,6 +172,7 @@ check_state
 
 check_file
 check_license
+check_pciscan
 check_raw
 check_rtime
 check_var

commit 49d5b97855fb28cf1bffb16eb4788959f75e6b98

RouterAccess: print PCI addresses as hex + 64bit
Previously, we prefixed the address with "0x" but actually we printed
decimal values! Additionally, we only printed the least significant 32
bits of the bus address. With more 64-bit systems, we now want to print
the full address.

Fixes: 7a35dd332dcfcca134658423a8553bbb4e86edb3
Signed-off-by: Patrick Bruenn <p.bruenn@beckhoff.com>

AdsLib/RouterAccess.cpp
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
 /**
-   Copyright (c) 2021 - 2022 Beckhoff Automation GmbH & Co. KG
+   Copyright (c) Beckhoff Automation GmbH & Co. KG
  */
 
 #include "RouterAccess.h"
 #include "Log.h"
 #include "wrap_endian.h"
 #include <array>
+#include <iomanip>
 #include <iostream>
 
 namespace bhf
@@ -55,9 +56,12 @@ struct SearchPciBusResNew {
 
 std::ostream &operator<<(std::ostream &os, const SearchPciSlotResNew &slot)
 {
+	const auto high = uint64_t(bhf::ads::letoh(slot.leBaseAddresses[1]));
+	const auto low = uint64_t(bhf::ads::letoh(slot.leBaseAddresses[0]));
+	const auto address = high << 32 | low;
 	return os << std::dec << bhf::ads::letoh(slot.leBusNumber) << ':'
-		  << bhf::ads::letoh(slot.leSlotNumber) << " @ 0x"
-		  << bhf::ads::letoh(slot.leBaseAddresses[0]);
+		  << bhf::ads::letoh(slot.leSlotNumber) << " @ 0x" << std::hex
+		  << std::setw(16) << std::setfill('0') << address;
 }
 
 RouterAccess::RouterAccess(const std::string &gw, const AmsNetId netid,